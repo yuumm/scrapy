872dd7b8d97683ba4f7360cba1bc2dd33b2c7467
yuumm <15802883812@163.com>
2019-10-14 10:52:27 +0800

    add distribute

--- quotetutorial/quotetutorial/settings.py
+++ quotetutorial/quotetutorial/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'quotetutorial.spiders'
 #USER_AGENT = 'quotetutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+ITEM_PIPELINES = {
 #    'quotetutorial.pipelines.QuotetutorialPipeline': 300,
-#}
+    'scrapy_redis.pipelines.RedisPipeline': 301
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +89,9 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+
+REDIS_URL = 'redis://lxz:123456@120.79.196.186:6379'
+
